61366790f64f33f557a428550a608805cb8413f8
FirozSkAhmad <you@example.com>
2023-03-27 14:18:00 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 export default function App() {
+  const [type, setType] = useState("password")
+  const [toggle, setToggle] = useState(true)
   return (
-    <div>
-    </div>
+    <>
+      <div id="container">
+        <div class="passInput"><span>Password:</span><input type={type} /></div>
+        <div class="icon" onClick={() => {
+          setToggle(!toggle);
+          (toggle) ? setType("text") : setType("password")
+        }}>{toggle ? <img src={"./eye_icon.png"} /> : <img src={"./eye_close.png"} />}</div>
+      </div>
+    </>
   );
 }
